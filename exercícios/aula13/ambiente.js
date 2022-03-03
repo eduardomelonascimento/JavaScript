a7d3258c80f9c2e8a4a1341b5cfe019b218b4191
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-03 13:52:52 -0300

    777

---
+++ exercícios/aula13/ambiente.js
@@ -0,0 +1,15 @@
+//var cont = 1
+
+for (var cont = 1; cont <= 5; cont++)  {
+    console.log(cont)
+}
+
+/* while (cont <= 5) {
+    console.log('Oi')
+    cont++
+} */
+
+/* do {
+    console.log('Oi')
+    cont++
+} while (cont <= 5) */
