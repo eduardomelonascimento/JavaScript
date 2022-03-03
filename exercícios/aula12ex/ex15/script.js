8b36464c61969e8a5bb5b6c22b7987f072484e4f
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-03 11:55:24 -0300

    777

--- exercícios/aula12ex/ex15/script.js
+++ exercícios/aula12ex/ex15/script.js
@@ -16,60 +16,74 @@ function verificar() {
                 genero = 'bebê'
                 img.src = 'imagens/baby-boy.jpg'
                 res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade <= 11) {
                 genero = 'garoto'
                 img.src = 'imagens/children-boy.jpg'
                 res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 18) {
                 genero = 'adolescente'
                 img.src = 'imagens/teenager-boy.jpg'
                 res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 30) {
                 genero = 'adulto'
                 img.src = 'imagens/young-adult-man.jpg'
                 res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 40) {
                 genero = 'adulto'
                 img.src = 'imagens/adult-man.jpg'
                 res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 55) {
                 genero = 'adulto'
                 img.src = 'imagens/adult-man2.jpg'
                 res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else {
                 genero = 'idoso'
                 img.src = 'imagens/old-man.jpg'
                 res.innerHTML `Detectamos um ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             }
         } else if (fsex[1].checked) {
             if (idade <= 3) {
-                genero = bebê
+                genero = 'bebê'
                 img.src = 'imagens/baby-girl.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade <= 11) {
                 genero = 'garota'
                 img.src = 'imagens/children-girl.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 18) {
                 genero = 'adolescente'
                 img.src = 'imagens/teenager-girl.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 30) {
                 genero = 'adulta'
                 img.src = 'imagens/young-adult-woman.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 40) {
                 genero = 'adulta'
                 img.src = 'imagens/adult-woman2.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else if (idade < 55) {
                 genero = 'adulta'
                 img.src = 'imagens/adult-woman.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             } else {
                 genero = 'idosa'
                 img.src = 'imagens/old-woman.jpg'
                 res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+                res.style.textAlign = 'center'
             }
         }
     }   
