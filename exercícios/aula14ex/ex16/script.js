97266b10d2f5bab1335d9b3a2951c17a227b02e4
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-03 16:37:10 -0300

    777

--- exercícios/aula14ex/ex16/script.js
+++ exercícios/aula14ex/ex16/script.js
@@ -1,17 +1,29 @@
-function carregar() {
-    var msg = document.querySelector('p#msg')
-    var img = document.querySelector('img#imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `<p>Agora são ${hora} horas</p>`
-    if (hora >= 0 && hora < 12){
-        img.src = 'imagens/manha.jpg'
-        document.body.style.background = '#7F8A68'
-    } else if (hora >= 12 && hora < 18) {
-        img.src = 'imagens/tarde.jpg'
-        document.body.style.background = '#F9BD90'
+function contar() {
+    var inicio = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('Faltam dados')
+        res.innerHTML = 'Impossível contar!'
     } else {
-        img.src = 'imagens/fotonoite.png'
-        document.body.style.background = '#254E80'
+        res.innerText = 'Contando: '
+        var ini = Number(inicio.value)
+        var end = Number(fim.value)
+        var step = Number(passo.value)
+        if (step <= 0) {
+            alert('Passo inválido! Considerando passo = 1')
+            step = 1
+        }
+        if (ini < end) {
+            for (var c = ini; c <= end; c += step) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for (var c = ini; c >= end; c-= step) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } 
+        res.innerHTML += '\u{1F3C1}'
     }
 }
---
+++ exercícios/aula14ex/ex17/script.js
@@ -0,0 +1,5 @@
+function calcular () {
+    var txtnum = document.getElementById('txtnum')
+    var num = Number.parseInt(txtnum.value)
+    res.innerHTML = `${num}`
+}
