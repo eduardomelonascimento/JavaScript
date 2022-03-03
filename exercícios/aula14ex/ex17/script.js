5948e77bdea4f250e2b493443c81100ae30b1ed3
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-03 18:35:52 -0300

    Update script.js

--- exercícios/aula14ex/ex17/script.js
+++ exercícios/aula14ex/ex17/script.js
@@ -1,5 +1,8 @@
 function calcular () {
     var txtnum = document.getElementById('txtnum')
     var num = Number.parseInt(txtnum.value)
+    if (txtnum.value.length == 0) {
+        alert('Digite um valor')
+    }
     res.innerHTML = `${num}`
 }
