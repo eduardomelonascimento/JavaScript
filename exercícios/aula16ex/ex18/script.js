a8b8f66cb0e0dc874d4014c3ada2767af099ff5c
eduardomelonascimento <nascimentomeloedu@gmail.com>
2022-03-04 11:43:04 -0300

    777

--- exercícios/aula16ex/ex18/script.js
+++ exercícios/aula16ex/ex18/script.js
@@ -1,10 +1,29 @@
-function adicionar() {
-    let cont = 0
-    let txtnum = document.getElementById('txtnum')
-    let num = Number(txtnum.value)
-    let numarray = []
-    numarray[cont](num)
-    alert(`${num}`)
-    alert(`${numarray}`)
-    cont++
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isnumber(n) {
+    if (Number(n) > 0 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function inlist(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar () {
+    if (isnumber(num.value) && !inlist(num.value)) {
+        window.alert('Tá flinstons')
+    } else {
+        window.alert('Valor inválido ou já inserido.')
+    }
 }
